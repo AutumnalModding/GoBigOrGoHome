10400916b1cc64fb221ae4defc4b1a5b0e8a4067
Lily Autumn <realprismaticyt@gmail.com>
2024-12-17 12:16:32 +1300

    Initial commit

---
+++ src/main/java/xyz/lilyflower/gbogh/GoBigOrGoHome.java
@@ -0,0 +1,9 @@
+package xyz.lilyflower.gbogh;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GoBigOrGoHome implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
